c2706c29a90fb6cf74ac279b43e7e363b05028bc
misganaw <misgandess@gmail.com>
2024-06-06 17:08:56 +0300

    removed unnedessary comment

--- internal/handler/ticket/ticket.go
+++ internal/handler/ticket/ticket.go
@@ -25,74 +25,6 @@ func Init(log slog.Logger, pmt module.Payment, tkt module.Ticket) handler.Ticket
 		tkt:     tkt,
 	}
 }
-
-// func (t *ticket) Buy(c *gin.Context) {
-// 	var user model.User
-// 	if err := c.ShouldBind(&user); err != nil {
-// 		t.log.Error("failed to bind user input", err)
-// 		c.HTML(http.StatusBadRequest, "error.html", err)
-// 		return
-// 	}
-// 	//validation
-// 	if err := user.Validate(); err != nil {
-// 		fmt.Println("email:", user.Email)
-// 		t.log.Error("invalid user input", err)
-// 		c.HTML(http.StatusBadRequest, "error.html", err)
-// 		return
-// 	}
-// 	err := t.module.CreateCheckoutSession(c, user)
-// 	if err != nil {
-// 		c.HTML(http.StatusInternalServerError, "error.html", err)
-// 		return
-// 	}
-
-// }
-
-// func (t *ticket) Notify(c *gin.Context) {
-
-// 	var notify model.Notification
-// 	if err := c.ShouldBindJSON(&notify); err != nil {
-// 		t.log.Error("failed to bind notification response body", err)
-// 		c.AbortWithStatusJSON(http.StatusBadRequest, err)
-// 		return
-// 	}
-
-// 	user, err := t.module.UpdatePaymentStatus(notify.TransactionStatus, notify.SessionID)
-// 	if err != nil {
-// 		t.log.Error("failed to update payment status", err)
-// 		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
-// 		return
-// 	}
-
-// 	pdf, err := t.module.GeneratePDFTicket(user)
-// 	if err != nil {
-// 		t.log.Error("Error generating PDF ticket:", err)
-// 		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
-// 		return
-// 	}
-// 	attachmentPath := fmt.Sprintf("./public/pdfs/attachment_%s.pdf", user.ID)
-// 	_, err = os.Create(attachmentPath)
-// 	if err != nil {
-// 		t.log.Error("error when creating attachment file for email")
-// 		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
-// 		return
-// 	}
-
-// 	if err := pdf.WritePdf(attachmentPath); err != nil {
-// 		t.log.Error("error when copying pdf to email attachment file", err)
-// 		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
-// 		return
-// 	}
-
-// 	wg := &sync.WaitGroup{}
-// 	wg.Add(2)
-// 	go t.sms.SendSms(user, wg)
-// 	go t.email.SendEmail(user, attachmentPath, wg)
-// 	wg.Wait()
-
-// 	c.Writer.WriteHeader(http.StatusOK)
-// }
-
 func (t *ticket) GetTicket(c *gin.Context) {
 	intentID := c.Param("intent_id")
 	fmt.Println("going to create ticket ------>")
@@ -131,13 +63,13 @@ func (t *ticket) GetTicket(c *gin.Context) {
 
 	c.File(ticketPath)
 
-	// if err := os.RemoveAll(ticketPath); err != nil {
-	// 	newError := model.Error{
-	// 		ErrCode: http.StatusInternalServerError,
-	// 		Message: "failed to remove pdf file after written to body",
-	// 	}
-	// 	t.log.Error("failed to remove pdf file", newError)
-	// 	c.JSON(newError.ErrCode, err)
-	// 	return
-	// }
+	if err := os.RemoveAll(ticketPath); err != nil {
+		newError := model.Error{
+			ErrCode: http.StatusInternalServerError,
+			Message: "failed to remove pdf file after written to body",
+		}
+		t.log.Error("failed to remove pdf file", newError)
+		c.JSON(newError.ErrCode, err)
+		return
+	}
 }
