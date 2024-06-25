93094d23488f8a3f0661230bdd0b1cf2ddf64b73
Misganaw <misgandess@gmail.com>
2024-06-25 23:43:58 +0300

    included storage mock with in ticket module test

--- internal/module/ticket/reserve_test.go
+++ internal/module/ticket/reserve_test.go
@@ -22,6 +22,30 @@ type reserveTicketTest struct {
 	err         error
 }
 
+type MockStorageTicket struct {
+	Tkt model.Ticket
+}
+
+func InitMock(tkt model.Ticket) *MockStorageTicket {
+	return &MockStorageTicket{Tkt: tkt}
+}
+func (m *MockStorageTicket) ReserveTicket(ticketNo, tripId int32) (model.Ticket, error) {
+
+	m.Tkt.Status = "Onhold"
+	return m.Tkt, nil
+
+}
+func (m *MockStorageTicket) GetTicket(tktNo, tripId int32) (model.Ticket, error) {
+	return m.Tkt, nil
+}
+func (m *MockStorageTicket) UnholdTicket(tktNo, tripId int32) (model.Ticket, error) {
+	if m.Tkt.Status == "Onhold" {
+		m.Tkt.Status = "Free"
+		return m.Tkt, nil
+	}
+	return model.Ticket{}, fmt.Errorf("failed to unhold ticket")
+}
+
 func TestReserveTicket(t *testing.T) {
 	logger := slog.Logger{}
 	store := storageTkt.InitMock(model.Ticket{})
@@ -76,9 +100,11 @@ func (r *reserveTicketTest) theUserShouldGetCheckoutUrl() error {
 }
 
 func (r *reserveTicketTest) ticketNumberOfBusNumberForTripOfIdIs(tktNo, busNo, tripId int, status string) error {
-	_, err := r.mockstorage.AddTicket(int32(tktNo), int32(busNo), int32(tripId), status)
-	if err != nil {
-		return err
+	r.mockstorage.Tkt = model.Ticket{
+		TripId:   int32(tripId),
+		TicketNo: int32(tktNo),
+		BusNo:    int32(busNo),
+		Status:   status,
 	}
 
 	return nil
