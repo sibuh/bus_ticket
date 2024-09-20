e78d8ed6c5eb3af0001fc1a2ea29e9d620f0d05e
nardoz <gnardos13@gmail.com>
2024-09-20 22:39:40 +0300

    complete scheduler

--- internal/module/scheduler/scheduler.go
+++ internal/module/scheduler/scheduler.go
@@ -1,13 +1,37 @@
 package scheduler
 
+import "time"
+
 type Scheduler struct {
-	Map map[string]chan string
+	smap map[string]chan string
+}
+
+func Init(smap map[string]chan string) *Scheduler {
+	return &Scheduler{smap}
 }
 
 func (s *Scheduler) Append(id string, ch chan string) {
-	s.Map[id] = ch
+	s.smap[id] = ch
+}
+
+func (s *Scheduler) Get(id string) chan string {
+	val, ok := s.smap[id]
+	if !ok {
+		return nil
+	}
+	return val
 }
 
 func (s *Scheduler) Remove(id string) {
-	delete(s.Map, id)
+	delete(s.smap, id)
+}
+
+func (s *Scheduler) Scheduler(id string, ch chan string, duration time.Duration, queryFunc func() error) {
+	select {
+	case <-ch:
+		s.Remove(id)
+		return
+	case <-time.After(duration * time.Second):
+		go queryFunc()
+	}
 }
