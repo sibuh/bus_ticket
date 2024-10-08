140eba22d4606fd10e4352243d6f2df2dc93f748
Misganaw <misgandess@gmail.com>
2024-10-08 17:59:34 +0300

    modified docker compose file

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Start CockroachDB in the background
+cockroach start-single-node --insecure --http-port=8080 --store=cockroach-data &
+
+# Wait for CockroachDB to be fully started
+sleep 3
+
+# Create the database
+cockroach sql --insecure --host=localhost:26257 -e "CREATE DATABASE IF NOT EXISTS mydb;"
+
+# Keep the container running
+wait
--- initiator/initiator.go
+++ initiator/initiator.go
@@ -35,24 +35,25 @@ func Initiate() {
 	server.Use(middleware.Cors())
 	v1 := server.Group("v1")
 	logger.Info("initiate database")
-	queries := InitDB(viper.GetString("dbConn"))
+	fmt.Println("connection string", fmt.Sprintf("postgres://%s@%s:%s/%s?sslmode=disable",
+		viper.GetString("DB_USER"),
+		viper.GetString("DB_HOST"),
+		viper.GetString("DB_PORT"),
+		viper.GetString("DB_NAME")))
+	queries := InitDB(fmt.Sprintf("postgres://%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME")))
 	logger.Info("intiating storage layer")
-	// storage := NewStorage(user.Init(logger, queries), event.Init(logger, queries), spmt.Init(logger, queries))
 	maker := paseto.NewPasetoMaker(viper.GetString("token.key"), viper.GetDuration("token.duration"))
 	mware := middleware.NewMiddleware(logger, maker, queries)
 	sc := schedule.Init()
 	module := NewModule(
-		muser.Init(
-			logger,
-			queries,
-			maker,
-		),
-		mtkt.Init(
-			logger,
+		muser.Init(logger, queries, maker),
+		mtkt.Init(logger,
 			paymentintegration.Init(logger, viper.GetString("payment.url")),
-			queries,
-			sc,
-		),
+			queries, sc),
 	)
 	err := godotenv.Load()
 	if err != nil {
